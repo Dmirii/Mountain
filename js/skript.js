51673eced59a922c1018af1ef94c687768c19728
Dmirii <63279146+Dmirii@users.noreply.github.com>
2020-05-03 22:25:01 +0300

    +

--- js/skript.js
+++ js/skript.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('.trips__body').slick();
+    $('.trips__body').slick({
+        adaptiveHeight: true,
+        
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        loop: true
+       
+        
+    });
 });
