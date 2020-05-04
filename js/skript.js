d4cff04af90e9d20c55fc60faabb62bcf39ba27b
Dmirii <63279146+Dmirii@users.noreply.github.com>
2020-05-04 09:46:53 +0300

    +

--- js/skript.js
+++ js/skript.js
@@ -3,8 +3,28 @@ $(document).ready(function(){
         adaptiveHeight: true,
         
         infinite: true,
-        speed: 300,
-        slidesToShow: 3
+        speed: 3000,
+        slidesToShow: 3,
+        autoplay:true,
+        easing:'easeInSine',
+        autoplaySpeed:5000,
+        centerMode:true,
+     
+        
+        responsive:[
+            {
+            breakepoint:1200,
+            settings:{
+                slidesToShow: 2, 
+                //centerPadding: '40px',
+            },
+            
+            breakepoint:400,
+                settings:{
+                    slidesToShow: 1
+                }
+
+        }]
      
        
         
